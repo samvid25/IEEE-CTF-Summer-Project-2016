8c60343f59ed0790a3858b0ce60fccbf76def450
Samvid <samvid25@yahoo.com>
2016-05-17 20:25:41 +0400

    Add names.py to prerequisites and remove names.py

--- names.py
+++
@@ -1,2 +0,0 @@
- names = {"Chinmay","Ajith","Arun","Nandini","Jeshvanth","Natasha","Prabhanjan","Rahul","Ranjith","Rohan","Me","Shreya","SKarthik","Sreeps","Swathi","Swati","Tarun","Yuvaraj","DHIR","Crowell","Ask123","Akshay"}
-sorted(names)
