89f54e75648d3597ee6b9a980b2a5b15281fb5ac
Samvid <samvid25@yahoo.com>
2016-05-17 20:24:36 +0400

    Add names.py

---
+++ prerequisites/names.py
@@ -0,0 +1,2 @@
+ names = {"Chinmay","Ajith","Arun","Nandini","Jeshvanth","Natasha","Prabhanjan","Rahul","Ranjith","Rohan","Me","Shreya","SKarthik","Sreeps","Swathi","Swati","Tarun","Yuvaraj","DHIR","Crowell","Ask123","Akshay"}
+sorted(names)
